1f7ac6aec894c710cf6363313370a81d9b33b30a
aleks.kositsyn <aleks.kositsyn@gmail.com>
2020-03-06 15:38:27 +0300

    changes in task_7

--- lesson_3/task_7.py
+++ lesson_3/task_7.py
@@ -2,24 +2,18 @@
 # Они могут быть как равны между собой (оба являться минимальными), так и различаться.
 import random
 
-SIZE = 10
+SIZE = 50
 MIN_ITEM = 1
-MAX_ITEM = 15
+MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
-if array[0] > array[1]:
-    min1 = 0
-    min2 = 1
-else:
-    min1 = 1
-    min2 = 0
-for i in range(2, 10):
-    if array[i] < array[min1]:
-        n = min1
-        min1 = i
-        if array[n] < array[min2]:
-            min2 = n
-    elif array[i] < array[min2]:
-        min2 = i
-print(f'1 минимальное число = {array[min1]} с индексом {min1}')
-print(f'2 минимальное число = {array[min2]} с индексом {min2}')
+
+for j in range(len(array)):
+    for i in range(len(array)-j-1):
+        if array[i] > array[i + 1]:
+            array[i], array[i + 1] = array[i + 1], array[i]
+
+# array.sort()
+print(array)
+print(f'1 минимальное число = {array[0]}')
+print(f'2 минимальное число = {array[1]}')
